5a577397a7973d3c8496dfb9196d7e3463f38b6e
Dan <danyboy_shaggy@hotmail.com>
2016-12-10 12:44:41 -0600

    Add more tests to ValidationsTest

--- src/test/java/com/zekke/webapp/util/ValidationsTest.java
+++ src/test/java/com/zekke/webapp/util/ValidationsTest.java
@@ -19,38 +19,83 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.fail;
 
 public class ValidationsTest {
 
+    private static final String ERROR_ARG_NULL_KEY = "error.arg.null";
+    private static final String ERROR_ARG_EMPTY_KEY = "error.arg.empty";
+    private static final String ERROR_ARG_BLANK_KEY = "error.arg.blank";
+    private static final String ARG_NAME = "arg";
+    private static final String STR_WITH_CONTENT = "A valid string";
+    private static final Object NON_NULL_OBJ = new Object();
+
     @Test
     public void shouldThrowNullPointerExceptionWhenArgIsNullInRequireNonNull() {
-        assertThatThrownBy(() -> Validations.requireNonNull(null, "error.arg.null", "arg")).isInstanceOf(NullPointerException.class);
+        assertThatThrownBy(() -> Validations.requireNonNull(null, ERROR_ARG_NULL_KEY, ARG_NAME)).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void shouldNotThrowAnyExceptionWhenArgIsNotNullInRequireNonNull() {
+        try {
+            Validations.requireNonNull(NON_NULL_OBJ, ERROR_ARG_NULL_KEY, ARG_NAME);
+        } catch (Exception ex) {
+            fail("Unexpected exception", ex);
+        }
     }
 
     @Test
     public void shouldThrowNullPointerExceptionWhenArgIsNullInRequireNonEmpty() {
-        assertThatThrownBy(() -> Validations.requireNonEmpty(null, "error.arg.empty", "arg")).isInstanceOf(NullPointerException.class);
+        assertThatThrownBy(() -> Validations.requireNonEmpty(null, ERROR_ARG_EMPTY_KEY, ARG_NAME)).isInstanceOf(NullPointerException.class);
     }
 
     @Test
     public void shouldThrowIllegalArgumentExceptionWhenArgIsEmptyInRequireNonEmpty() {
-        assertThatThrownBy(() -> Validations.requireNonEmpty(Strings.EMPTY, "error.arg.empty", "arg")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validations.requireNonEmpty(Strings.EMPTY, ERROR_ARG_EMPTY_KEY, ARG_NAME)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test(dataProvider = "nonEmptyStrings")
+    public void shouldNotThrowAnyExceptionWhenArgIsNotEmptyInRequireNonEmpty(String nonEmpty) {
+        try {
+            Validations.requireNonEmpty(nonEmpty, ERROR_ARG_NULL_KEY, ARG_NAME);
+        } catch (Exception ex) {
+            fail("Unexpected exception", ex);
+        }
     }
 
     @Test
     public void shouldThrowNullPointerExceptionWhenArgIsNullInRequireNonBlank() {
-        assertThatThrownBy(() -> Validations.requireNonBlank(null, "error.arg.blank", "arg")).isInstanceOf(NullPointerException.class);
+        assertThatThrownBy(() -> Validations.requireNonBlank(null, ERROR_ARG_BLANK_KEY, ARG_NAME)).isInstanceOf(NullPointerException.class);
     }
 
     @Test(dataProvider = "blankStrings")
     public void shouldThrowIllegalArgumentExceptionWhenArgIsBlankInRequireNonBlank(String blank) {
-        assertThatThrownBy(() -> Validations.requireNonBlank(blank, "error.arg.blank", "arg")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validations.requireNonBlank(blank, ERROR_ARG_BLANK_KEY, ARG_NAME)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void shouldNotThrowAnyExceptionWhenArgIsNotEmptyInRequireNonBlank() {
+        try {
+            Validations.requireNonBlank(STR_WITH_CONTENT, ERROR_ARG_NULL_KEY, ARG_NAME);
+        } catch (Exception ex) {
+            fail("Unexpected exception", ex);
+        }
     }
 
     @DataProvider
     public Object[][] blankStrings() {
         return new Object[][]{
                 {Strings.EMPTY},
+                {Strings.BLANK_SPACE},
+                {Strings.NEW_LINE},
+                {Strings.TAB},
+        };
+    }
+
+    @DataProvider
+    public Object[][] nonEmptyStrings() {
+        return new Object[][]{
+                {STR_WITH_CONTENT},
                 {Strings.BLANK_SPACE},
                 {Strings.NEW_LINE},
                 {Strings.TAB},
